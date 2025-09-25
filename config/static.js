7c4fed97f532255ffc7a677ba7b61055f3db4530
DelanPinto <141720978+DelanPinto@users.noreply.github.com>
2025-09-25 22:04:47 +0530

    Add files via upload

---
+++ config/static.js
@@ -0,0 +1,7 @@
+// Serve static files from the public directory
+const express = require('express');
+const path = require('path');
+
+module.exports = function(app) {
+  app.use(express.static(path.join(__dirname, '../public')));
+};
